e8385a61a21a2592c0c03bb571a713396296d612
Partha Chakraborty <partha.c1999@gmail.com>
2023-01-06 17:13:39 +0530

    Used local storage for storing user logged in data

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const userLoggedInData = localStorage.getItem('isLoggedIn');
+  if (userLoggedInData === '1') {
+    setIsLoggedIn(true);
+  }
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn','1');
     setIsLoggedIn(true);
   };
 
