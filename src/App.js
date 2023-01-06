30ae408f90ea2256833cb43d4c3b773028f099eb
Partha Chakraborty <partha.c1999@gmail.com>
2023-01-06 17:18:52 +0530

    Used useEffect for optimization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,15 +7,17 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const userLoggedInData = localStorage.getItem('isLoggedIn');
-  if (userLoggedInData === '1') {
-    setIsLoggedIn(true);
-  }
 
+  useEffect(() => {
+    const userLoggedInData = localStorage.getItem('isLoggedIn');
+    if (userLoggedInData === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn','1');
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
